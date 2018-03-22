759fa244d078f3f16d88d6dd85916a160072e06d
joseantogt <30443895+joseantogt@users.noreply.github.com>
2018-03-22 16:48:43 -0600

    Add files via upload

--- src/ModifyDB.java
+++ src/ModifyDB.java
@@ -1,7 +1,23 @@
+import java.io.File;
+
 public class ModifyDB {
 
-    public void ModifyNameDirectory(String nameDB, String newNameDB, String actualDB){
+    public String ModifyNameDirectory(String nameDB, String newNameDB){
+        File file = new File("C:\\Users\\Jose Ramirez\\Downloads\\Test\\" + nameDB );
 
+        // Comprueba si el directoria ya existe
+        if (!file.exists()) {
+            System.out.println(" La Base de Datos no existe ");
+            return " La Base de Datos no existe ";
+        }
+
+        else{
+            File filerename = new File ("C:\\Users\\Jose Ramirez\\Downloads\\Test\\" + newNameDB);
+            file.renameTo(filerename);
+            System.out.println("El cambio de nombre se realizo con exito");
+            return "El cambio de nombre se realizo con exito";
+
+        }
 
     }
 }
